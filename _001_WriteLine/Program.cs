523df221562157ab5af91aa1bb1d5e2871abceb1
ChangJoon_Lee <kelvinjr177@icloud.com>
2023-01-14 18:59:30 +0900

    2nd commit

---
+++ _001_WriteLine/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _001_WriteLine
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 1234;
+            double d = 5.6789;
+            string s = "문자열";
+
+            Console.WriteLine(i);
+            Console.WriteLine(d);
+            Console.WriteLine(s);
+        }
+    }
+}
